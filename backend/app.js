750373be3d7361fff30ac0e11bb55d5f71e7699d
Tessa Tapmongkol <ttapmong@calpoly.edu>
2023-01-13 22:31:17 -0800

    lemme update that .env rq

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
+require("dotenv").config();
 
-mongoose.connect(process.env.DATABASE_URL, (req, res) => {
+mongoose.connect(process.env.MONGO_URI, (req, res) => {
     console.log("connected to database")
 });
 
 const app = express();
 
 app.get("/travel-posts", (req, res) => {
-
+    console.log("travel posts here")
 })
 
 app.get("/books-posts", (req, res) => {
-
+    console.log("book posts here")
 })
 
 app.listen(3000, () => {
