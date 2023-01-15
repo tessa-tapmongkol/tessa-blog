09ba913631ee7dc0be383c7e245f1853b9da842a
Tessa Tapmongkol <ttapmong@calpoly.edu>
2023-01-15 01:15:53 -0800

    routes set up

---
+++ frontend/src/pages/Admin/Admin.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Admin = () => {
+    return (
+        <>
+            <div>
+                <h2>Admin</h2>
+            </div>
+        </>
+    )
+}
+
+export default Admin;
