d47ae2be8aebad6a1baaa8d69633ae9ebdfa77be
Tessa Tapmongkol <ttapmong@calpoly.edu>
2023-01-16 01:56:07 -0800

    i added a god damn earth

--- frontend/src/pages/Travel/Travel.js
+++ frontend/src/pages/Travel/Travel.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Navbar from '../../components/Navbar/Navbar';
 import { categories } from '../../constants/categories';
-import ReactGlobe from "react-globe";
+import Globe from "react-globe.gl";
+import "./Travel.css";
 
 const Travel = () => {
     return (
-        <>
+        <div id="travel">
             <Navbar type={categories.TRAVEL} />
-            <ReactGlobe height="100vh" width="100vw" />
-        </>
+            <Globe height="100vh" width="100vw" globeImageUrl={'//unpkg.com/three-globe/example/img/earth-blue-marble.jpg'} />
+        </div>
     )
 }
 
